99e955481a596338e0e0caf62fc4590578a57f0b
dymlopecillo202400228 <dymlopecillo.202400228@gmail.com>
2026-04-10 10:09:20 +0800

    pogi ko

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const students =[ 
+    {
+        Id : 1,
+        name: "gaton",
+        yearlevel : 1,
+
+    }
+]
+
+
+
+
+app.get('/api/student', (req, res) => {
+    res.json(students)
+})
+
+
+app.post('/api/student', (req, res) => {
+    const {name , yearlevel} = req.body
+    const  newStudent = {name, yearlevel}
+    students.push(newStudent)
+
+    res.status(201).json({
+        message: "Student added successfully",
+        student: newStudent
+    })
+})
+
+    
+app.listen(port, () => {
+    console.log(`server reunning on https://localhost:${port}`)
+})
